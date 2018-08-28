c734c7fa5d8c90adf24f5b83e19ec20c7aa83701
andre <ferreira.andreluiz@gmail.com>
2018-08-28 13:34:28 -0300

    init

---
+++ spring-apis-netflix-eureka-server/src/main/java/com/springApisNetlix/eureka/SpringApisNetflixEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.springApisNetlix.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringApisNetflixEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApisNetflixEurekaServerApplication.class, args);
+	}
+}
---
+++ spring-apis-netflix-produto/src/main/java/com/springApisNetlix/produto/SpringApisNetflixProdutoApplication.java
@@ -0,0 +1,12 @@
+package com.springApisNetlix.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApisNetflixProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApisNetflixProdutoApplication.class, args);
+	}
+}
